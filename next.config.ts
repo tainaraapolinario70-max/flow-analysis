bdbe6ad43e0552f1b29689f18ca20616f520db8f
tainaraapolinario70-max <tainaraapolinario70@gmail.com>
2026-09-24 12:45:13 -0300

    Update Next.js configuration for output and images

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
